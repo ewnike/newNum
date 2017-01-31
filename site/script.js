eeae963695d517115d726e8d72ed0281d23e6c8a
ewnike <wpltd5@yahoo.com>
2017-01-30 20:52:41 -0600

    solution

--- site/script.js
+++ site/script.js
@@ -1,1 +1,10 @@
-function numbersOnly(){
+function numbersOnly(arr){
+  var newArr = [];
+  for(var i = 0; i < arr.length; i++){
+    if(typeof(arr[i])==="number"){
+      newArr.push(arr[i]);
+    }
+  }
+  console.log(newArr);
+}
+numbersOnly([1,"r",3.5, "p"])
